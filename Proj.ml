b7fb565cb872e7bf6ee79dd36ddb3f1aaa14b0f9
totoXV <113538292+totoXV@users.noreply.github.com>
2022-12-11 19:50:56 +0100

    2.2 et debut 2.1

--- Proj.ml
+++ Proj.ml
@@ -8,7 +8,10 @@ type b = Zero | Un;;
 
 type e =
   | Variable of v
-  | Booleen of b;;
+  | Booleen of b
+  | Conjonction of e * e
+  | Disjonction of e * e
+  | Negation of e;;  
 
 type i =
   | Sequence of i * i
@@ -65,3 +68,99 @@ and
   | While of e * s
   | If of e * s * s
   | EpsilonI;;
+
+
+(*EXO 2.1*)
+
+let list_of_string s =
+  let n = String.length s in
+  let rec boucle i =
+    if i = n then []
+    else s.[i] :: boucle (i+1)
+  in boucle 0;;
+
+
+type analist = char list -> char list
+type ('r, 'term) ranalist = 'term list -> 'r * 'term list;;
+exception Echec
+
+let terminal c : analist = fun l -> match l with
+  | x :: l when x = c -> l
+  | _ -> raise Echec
+
+let terminal_cond (p : char -> bool) : analist = fun l -> match l with
+  | x :: l when p x -> l
+  | _ -> raise Echec
+
+let epsilon : analist = fun l -> l
+
+let ( -->) (a : analist) (b : analist) : analist =
+  fun l -> let l = a l in b l
+
+let (-|) (a1 : analist) (a2 : analist) : analist =
+  fun l -> try a1 l with Echec -> a2 l
+
+let epsilon_res (info : 'res) : ('res, 'term) ranalist =
+  fun l -> (info, l)
+  
+let terminal_res (f : 'term -> 'res option) : ('res, 'term) ranalist =
+  fun l -> match l with
+  | x :: l -> (match f x with Some y -> y, l | None -> raise Echec)
+  | _ -> raise Echec
+  
+let (+|) (a : ('res, 'term) ranalist) (b : ('res, 'term) ranalist) : ('res, 'term) ranalist =
+  fun l -> try a l with Echec -> b l
+
+let ( -+>) (a : analist) (b : ('res, 'term) ranalist) : ('res, 'term) ranalist =
+  fun l -> let l = a l in b l
+
+let (+->) (a : ('res, 'term) ranalist) (b : analist) : analist =
+  fun l -> let (x, l) = a l in b l
+
+let (++>) (a : ('resa, 'term) ranalist) (b : 'resa -> ('resb, 'term) ranalist) : ('resb, 'term) ranalist =
+  fun l -> let (x, l) = a l in b x l
+
+let pa_C = (terminal '1' -| terminal '0');;
+
+let pa_V = (terminal 'a' -| terminal 'b' -| terminal 'c' -| terminal 'd');;
+
+let pa_CV = (pa_C -| pa_V);;
+
+let pa_A = (pa_V --> terminal ':' --> terminal '=' --> pa_CV);;
+
+(* EXO 2.2 *)
+
+
+type state = (var*int) list;;
+
+let rec (get : var -> state -> int) = fun c s ->
+  match s with
+  |(var,value) :: suite -> if var=c then value else get c suite
+  |[] -> raise Echec;;
+
+ let rec (update: state -> int -> var -> state) = fun s i v ->
+   match s with
+   | [] -> (v,i) :: []
+   | (var,value) :: suite -> if var=v then (v,i)::suite else (var,value)::(update suite i v)
+ ;;
+  
+let rec eval (s:state) (exp:e) : b = 
+  match exp with
+  | Variable v -> get s v
+  | Boolean b -> b
+  | Conjonction (e1, e2) -> 
+  match b1 with
+    |Zero -> Zero
+    |Un -> 
+    match b2 with
+          |Zero -> Zero
+          |Un -> Un
+  | Disjonction (e1, e2) -> 
+  match b1 with
+    |Un -> Un
+    |Zero -> 
+    match b2 with
+          |Zero -> Zero
+          |Un -> Un
+  | Negation e -> b = if b1=Un then Zero else Un
+;;
